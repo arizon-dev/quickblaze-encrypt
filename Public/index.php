c2b6325e81c4067ce5427fedc31317bd93f5730d
Axton <enquires@axtonprice.com>
2022-04-15 22:31:51 +0100

    Dark Mode Update

--- Public/index.php
+++ Public/index.php
@@ -36,6 +36,15 @@
         </form>
     </main>
 
+    <!-- Dark Mode Widget -->
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+    <script>
+        function addDarkmodeWidget() {
+            new Darkmode().showWidget();
+        }
+        window.addEventListener('load', addDarkmodeWidget);
+    </script>
+
 </body>
 
 </html>
