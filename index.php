70894e020690f661eb3c9679ccd7dab25968be9b
Axton <enquires@axtonprice.com>
2022-04-17 00:20:09 +0100

    ✅ Updated URL Handling and Rendering

--- index.php
+++ index.php
@@ -2,15 +2,9 @@
 /* Initialise the Application */
 $url = $_SERVER['REQUEST_URI'];
 $url = substr($url, strrpos($url, '/') + 1);
-if(strpos($url, '?') !== false) $url = substr($url, 0, strpos($url, "?"));
+if (strpos($url, '?') !== false) $url = substr($url, 0, strpos($url, "?"));
 
 // /* Initialise Displays */
-if ($url == "") {
-    /* Primary Display Page */
-    require("./Modules/Functions.php");
-    setupDatabase(); // Initialise Database
-    require("./Public/index.php");
-}
 if ($url == "processForm") {
     /* Form Submission Handler */
     require("./Modules/Functions.php");
@@ -23,4 +17,31 @@ if ($url == "view") {
     require("./Public/view.php");
     return;
 }
-?>
+if ($url == "") {
+    /* Primary Display Page */
+    require("./Modules/Functions.php");
+    setupDatabase(); // Initialise Database
+    require("./Public/index.php");
+} elseif ($url == "404") {
+    /* Not Found Page */
+    require("./Modules/Functions.php");
+    require("./Public/Error/404.html");
+    return;
+} elseif ($url == "403") {
+    /* Not Found Page */
+    require("./Modules/Functions.php");
+    require("./Public/Error/403.html");
+    return;
+} else {
+    if ($url == "500") {
+        /* Server Error Page */
+        require("./Modules/Functions.php");
+        require("./Public/Error/500.html");
+        return;
+    } else {
+        /* Not Found Page */
+        require("./Modules/Functions.php");
+        require("./Public/Error/404.html");
+        return;
+    }
+}
