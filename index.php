e6f58495e94d8727c75ed73c0bb4fc5e3ee396c3
Axton <enquires@axtonprice.com>
2022-04-17 00:15:14 +0100

    ✅ Updated URL Handling and Rendering

--- index.php
+++ index.php
@@ -10,46 +10,17 @@ if ($url == "") {
     require("./Modules/Functions.php");
     setupDatabase(); // Initialise Database
     require("./Public/index.php");
+}
+if ($url == "processForm") {
+    /* Form Submission Handler */
+    require("./Modules/Functions.php");
+    require("./Public/processForm.php");
     return;
-} else {
-    if ($url == "view") {
-        /* View Message Page */
-        require("./Modules/Functions.php");
-        require("./Public/view.php");
-        return;
-    } else {
-        if ($url == "processForm") {
-            /* Form Submission Handler */
-            require("./Modules/Functions.php");
-            require("./Public/processForm.php");
-            return;
-        } else {
-            if ($url == "404") {
-                /* Not Found Page */
-                require("./Modules/Functions.php");
-                require("./Public/Error/404.html");
-                return;
-            } else {
-                if ($url == "403") {
-                    /* No Permission Page */
-                    require("./Modules/Functions.php");
-                    require("./Public/Error/403.html");
-                    return;
-                } else {
-                    if ($url == "500") {
-                        /* Server Error Page */
-                        require("./Modules/Functions.php");
-                        require("./Public/Error/500.html");
-                        return;
-                    } else {
-                        /* Not Found Page */
-                        require("./Modules/Functions.php");
-                        require("./Public/Error/404.html");
-                        return;
-                    }
-                }
-            }
-        }
-    }
+}
+if ($url == "view") {
+    /* View Message Page */
+    require("./Modules/Functions.php");
+    require("./Public/view.php");
+    return;
 }
 ?>
