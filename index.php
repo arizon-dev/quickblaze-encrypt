d23b37f2575d06c9ccf8a5448dd19a80defbffcd
Axton <enquires@axtonprice.com>
2022-04-16 22:58:50 +0100

    ✅ Updated URL Handling and Rendering

--- index.php
+++ index.php
@@ -4,47 +4,50 @@ $url = $_SERVER['REQUEST_URI'];
 $url = substr($url, strrpos($url, '/') + 1);
 $url = strstr($url, '?', true);
 
-/* Initialise Scripts */
-if ($url == "view") {
-    require("./Modules/Functions.php");
-    require("./Public/view.php");
-    return;
-}
-if ($url == "processForm") {
-    require("./Modules/Functions.php");
-    require("./Public/processForm.php");
-    return;
-}
-
-/* Default Page */
+/* Initialise Displays */
 if ($url == "") {
+    /* Primary Display Page */
     require("./Modules/Functions.php");
     setupDatabase(); // Initialise Database
     require("./Public/index.php");
     return;
 } else {
-    if ($url == "404") {
-        /* Not Found Page */
+    if ($url == "view") {
+        /* View Message Page */
         require("./Modules/Functions.php");
-        require("./Public/Error/404.html");
+        require("./Public/view.php");
         return;
     } else {
-        if ($url == "403") {
-            /* Not Found Page */
+        if ($url == "processForm") {
+            /* Form Submission Handler */
             require("./Modules/Functions.php");
-            require("./Public/Error/403.html");
+            require("./Public/processForm.php");
             return;
-        } else{
-            if ($url == "500") {
-                /* Server Error Page */
-                require("./Modules/Functions.php");
-                require("./Public/Error/500.html");
-                return;
-            } else {
+        } else {
+            if ($url == "404") {
                 /* Not Found Page */
                 require("./Modules/Functions.php");
                 require("./Public/Error/404.html");
                 return;
+            } else {
+                if ($url == "403") {
+                    /* No Permission Page */
+                    require("./Modules/Functions.php");
+                    require("./Public/Error/403.html");
+                    return;
+                } else {
+                    if ($url == "500") {
+                        /* Server Error Page */
+                        require("./Modules/Functions.php");
+                        require("./Public/Error/500.html");
+                        return;
+                    } else {
+                        /* Not Found Page */
+                        require("./Modules/Functions.php");
+                        require("./Public/Error/404.html");
+                        return;
+                    }
+                }
             }
         }
     }
