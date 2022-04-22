b4f50e665462b4ef8cfe0647ecad069d221d72fc
Axton <enquires@axtonprice.com>
2022-04-22 23:08:08 +0100

    :bug: Translation System Testing

--- Public/translate.php
+++ Public/translate.php
@@ -1,36 +1,8 @@
 <?php
-// require("./vendor/autoload.php");
-require './vendor/delfimov/translate/src/Translate.php';
-require './vendor/delfimov/translate/src/Loader/LoaderInterface.php';
-require './vendor/delfimov/translate/src/Loader/PhpFilesLoader.php';
+function translate($q, $sl, $tl){
+    $res= file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&sl=".$sl."&tl=".$tl."&hl=hl&q=".urlencode($q), $_SERVER['DOCUMENT_ROOT']."/transes.html");
+    $res=json_decode($res);
+    return $res[0][0][0];
+}
 
-use DElfimov\Translate\Translate;
-use DElfimov\Translate\Loader\PhpFilesLoader;
-use Monolog\Logger; // PSR-3 logger, not required  
-use Monolog\Handler\StreamHandler;
-
-$log = new Logger('Translate');
-$log->pushHandler(new StreamHandler('./Modules/composerlogs/latest.log', Logger::WARNING));
-
-$t = new Translate(
-    new PhpFilesLoader(__DIR__ . "/messages"),
-    [
-        "default" => "en",
-        "available" => ["en", "ru"],
-    ],
-    $log // optional
-);
-
-$num = rand(0, 100);
-
-$t->setLanguage("en"); // this is not required, language will be auto detected with Accept-Language HTTP header
-echo $t->t('some string') . "\n\n"; // or $t('some string');
-echo $t->plural('%d liters', $num) . "\n\n";
-echo $t->plural("The %s contains %d monkeys", $num, ['tree', $num]) . "\n\n";
-
-$num = rand(0, 100);
-
-$t->setLanguage("ru");
-echo $t->t('some string')."\n\n"; // or $t('some string');
-echo $t->plural('%d liters', $num) . "\n\n";
-echo $t->plural("The %s contains %d monkeys", $num, ['tree', $num]) . "\n\n";
+echo translate($_GET["text"], "en", "is");
