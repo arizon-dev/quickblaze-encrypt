76b178c8e696979e49bff33de528bf930d6c74e9
Axton <enquires@axtonprice.com>
2022-04-22 23:00:51 +0100

    :bug: Commit: Error Reversal

--- index.php
+++ index.php
@@ -5,14 +5,27 @@ $url = substr($url, strrpos($url, '/') + 1);
 if (strpos($url, '?') !== false) $url = substr($url, 0, strpos($url, "?"));
 
 // /* Initialise Displays */
+if ($url == "processForm") {
+    /* Form Submission Handler */
+    require("./Modules/Functions.php");
+    checkDatabase(); // Check database
+    require("./Public/processForm.php");
+    return;
+}
+if ($url == "view") {
+    /* View Message Page */
+    require("./Modules/Functions.php");
+    checkDatabase(); // Check database
+    require("./Public/view.php");
+    return;
+}
 if ($url == "") {
     /* Primary Display Page */
     require("./Modules/Functions.php");
     checkDatabase(); // Check database
     setupDatabase(); // Initialise Database
     require("./Public/index.php");
-}
-if ($url == "404") {
+} elseif ($url == "404") {
     /* Not Found Page */
     require("./Modules/Functions.php");
     require("./Public/Error/404.html");
@@ -22,20 +35,25 @@ if ($url == "404") {
     require("./Modules/Functions.php");
     require("./Public/Error/403.html");
     return;
-} elseif ($url == "500") {
-    /* Server Error Page */
-    require("./Modules/Functions.php");
-    require("./Public/Error/500.html");
-    return;
 } else {
-    /* Not Found Page */
-    require("./Modules/Functions.php");
-    require("./Public/Error/404.html");
-    return;
+    if ($url == "500") {
+        /* Server Error Page */
+        require("./Modules/Functions.php");
+        require("./Public/Error/500.html");
+        return;
+    } else {
+        /* Not Found Page */
+        require("./Modules/Functions.php");
+        require("./Public/Error/404.html");
+        return;
+    }
 }
 
-/* View Requested Page */
-require("./Modules/Functions.php");
-checkDatabase(); // Check database
-require("./Public/$url.php");
-return;
+// Testing
+if ($url == "translate") {
+    /* View Message Page */
+    require("./Modules/Functions.php");
+    checkDatabase(); // Check database
+    require("./Modules/TranslationCore.php");
+    return;
+}
