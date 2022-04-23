69cec78a8edcf25f6b9dd849fea5505a97916778
Axton <enquires@axtonprice.com>
2022-04-23 17:22:59 +0100

    🐛 Commit: Translation Features

--- Public/translate.php
+++ Public/translate.php
@@ -1,15 +1,19 @@
 <?php
-function translate($q, $sl, $tl){
+function translate($q, $sl){
+    $config = json_decode(file_get_contents("./.version", true), true);
+    if($config["LANGUAGE"] == "auto"){
+        $tl = getBrowserLang();
+    } else{
+        $tl = $config["LANGUAGE"];
+    }
     $res= file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&sl=".$sl."&tl=".$tl."&hl=hl&q=".urlencode($q), $_SERVER['DOCUMENT_ROOT']."/transes.html");
     $res=json_decode($res);
     return $res[0][0][0];
 }
 function getBrowserLang(){
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-    $acceptLang = ['fr', 'it', 'en']; 
-    $lang = in_array($lang, $acceptLang) ? $lang : 'en';
     return $lang;
 }
-echo translate($_GET["text"], "en", getBrowserLang());
+echo translate($_GET["text"], "en");
 echo "<Br>";
 echo getBrowserLang();
