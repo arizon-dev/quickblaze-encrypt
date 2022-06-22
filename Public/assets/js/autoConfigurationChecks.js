1810aa6d96db57db5606141dc4de9606268ecf91
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-06-22 16:57:25 +0100

    Syntax Refactor
    Refactored syntax for autoConfigurationChecks.js

--- Public/assets/js/autoConfigurationChecks.js
+++ Public/assets/js/autoConfigurationChecks.js
@@ -1,12 +1,13 @@
-function autoRecheckConfig(pageName) {
+const autoRecheckConfig = (pageName) => {
     fetch(`dataProcessing?action=checkConfig`).then(response => response.json()).then(data => {
         log(`[RE-CHECK] Server responded with ${data.response}`);
-        if(data.response == "true"){
+        if (data.response == "true") {
             window.location.replace("./");
-        }
+        };
     });
     setTimeout(autoRecheckConfig, 3000);
-}
-var path = window.location.pathname;
-var page = path.split("/").pop();
+};
+
+let path = window.location.pathname,
+    page = path.split("/").pop();
 autoRecheckConfig(page);
