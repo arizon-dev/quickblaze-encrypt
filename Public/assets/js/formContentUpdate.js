16ab9f1620198c54d33aa9d70dd5bce103eaa0c9
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-06-22 17:03:01 +0100

    Syntax Refactor
    Refactored syntax for formContentUpdate.js

--- Public/assets/js/formContentUpdate.js
+++ Public/assets/js/formContentUpdate.js
@@ -1,34 +1,40 @@
-function updateFormDisplay() {
+const updateFormDisplay = () => {
     const formvalue = document.getElementById("inputtextbot").value; // Assign variable to the current value of the textbox
-    $('#form_input').fadeOut('fast'); log(`No longer showing 'form_input' element`); // fade out previous content
+    
+    $('#form_input').fadeOut('fast'); // fade out previous content
+    log(`No longer showing 'form_input' element`);
+    
     fetch(`dataProcessing?data=${formvalue}`).then(response => response.json()).then(data => {
         log(`Server responsed with '${data.response}'`);
         document.getElementById("submissiontextbox").value = `${window.location}view?key=${data.response}`; log(`Updated 'submissiontextbox.value'`); // Set text box to view message URL
         document.getElementById("submissiontextbox").innerHTML = `${window.location}view?key=${data.response}`; log(`Updated 'submissiontextbox.innerHTML'`); // Set text box to view message URL
     });
-    setTimeout(function () {
+    setTimeout(() => {
         $('#form_submission').fadeIn('fast'); log(`Now showing 'form_submission' element`); // fade in new content
     }, 200);
-}
+};
 
-function updateViewDisplay() {
+const updateViewDisplay = () => {
     $('#form_confirmation').fadeOut('fast'); log(`No longer showing 'form_confirmation' element`); // fade out previous content
-    var key = new URL(window.location).searchParams.get("key"); log(`Got key variable from url -> ${key}`); // Get key variable from URL; replacing PHP usage
+
+    let key = new URL(window.location).searchParams.get("key"); // Get key variable from URL; replacing PHP usage
+    log(`Got key variable from url -> ${key}`);
+
     fetch(`dataProcessing?action=decrypt&key=${key}`).then(response => response.json()).then(data => {
         if (data.response == "") {
             showSnackBar('snackbarError');
             $('#form_error').fadeIn('fast'); log(`Now showing 'form_error' element`); // fade in new content
-            log(`Encryption not found; redirecting in 2s`)
-            setTimeout(function () {
+            log(`Encryption not found; redirecting in 2s`);
+            setTimeout(() => {
                 window.location.replace("./"); // Redirect to home page
             }, 2000);
         } else {
-            log(`Server responded with '${data.response}'`);
             document.getElementById("valuetextbox").value = data.response; log(`Updated 'valuetextbox.value'`); // Set text box to decrypted message
             document.getElementById("valuetextbox").innerHTML = data.response; log(`Updated 'valuetextbox.innerHTML'`); // Set text box to decrypted message
-            setTimeout(function () {
+            setTimeout(() => {
                 $('#form_content').fadeIn('fast'); log(`Now showing 'form_content' element`); // fade in new content
             }, 200);
-        }
+            log(`Server responded with '${data.response}'`);
+        };
     });
-}
+};
