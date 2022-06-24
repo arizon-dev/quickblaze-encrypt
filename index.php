50fcb858b8ba68dbf6c8ad4885fda2ba6f118597
Axton <enquires@axtonprice.com>
2022-06-24 12:11:38 +0100

    🐛 v1.1.10-dev Minor Commit
    Co-Authored-By: Brendan Price <56489848+brendanprice2003@users.noreply.github.com>

--- index.php
+++ index.php
@@ -7,37 +7,37 @@ if (strpos($url, '?') !== false) $url = substr($url, 0, strpos($url, "?"));
 /* Initialise Displays */
 if ($url == "dataProcessing") {
     /* Form Submission Handler */
-    require("./Modules/modules.php");
+    require("./Modules/functions.php");
     require("./Public/dataProcessing.php");
     return;
 }
 if ($url == "view") {
     /* View Message Page */
-    require("./Modules/modules.php");
+    require("./Modules/functions.php");
     require("./Public/view.php");
     return;
 }
 if ($url == "") {
     /* Primary Display Page */
-    require("./Modules/modules.php");
+    require("./Modules/functions.php");
     setupStorageMethod(); // Check database
     require("./Public/index.php");
 } elseif ($url == "404") {
     /* Not Found Page */
-    require("./Modules/modules.php");
+    require("./Modules/functions.php");
     return require("./Public/error/404.php");
 } elseif ($url == "403") {
     /* Not Found Page */
-    require("./Modules/modules.php");
+    require("./Modules/functions.php");
     return require("./Public/error/403.php");
 } else {
     if ($url == "500") {
         /* Server Error Page */
-        require("./Modules/modules.php");
+        require("./Modules/functions.php");
         return require("./Public/error/500.php");
     } else {
         /* Not Found Page */
-        require("./Modules/modules.php");
+        require("./Modules/functions.php");
         return require("./Public/error/404.php");
     }
 }
