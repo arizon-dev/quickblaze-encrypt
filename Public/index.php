3b2fb2e5a66f3c5750a24e315b7999e610338448
Axton <axton@arizon.dev>
2023-04-27 11:50:54 +0100

    🐛 v1.1.16 Feature IMP Commit

--- Public/index.php
+++ Public/index.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="./assets/img/favicon-100x100.png">
-    <meta name="description" content="An extremely simple, one-time view encrypted message system. Send anybody passwords, or secret messages on a one-time view basis.">
+    <link rel="icon" type="image/x-icon" href="<?= getInstallationPath() ?>/Public/assets/img/favicon-100x100.png">
+    <meta name="description" content="<?= translate("An extremely simple, one-time view encrypted message system. Send anybody passwords, or secret messages on a one-time view basis.") ?>">
     <title>QuickBlaze</title>
 
     <!-- Site CSS -->
-    <link href="./assets/css/style.css" rel="stylesheet">
+    <link href="<?= getInstallationPath() ?>/Public/assets/css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css">
 </head>
@@ -26,6 +26,7 @@
             <!-- Main Form Content -->
             <div id="form_input">
                 <textarea type="text" class="form-control size-max" id="input_text_box" placeholder="<?= translate("Enter your secret message!") ?>" required></textarea>
+
                 <input class="form-control" type="password" id="input_password" placeholder="<?= translate("Enter decryption password") ?>"></input>
                 <br>
                 <button type="button" class="btn btn-primary submit-button darkmode-ignore" onclick="updateFormDisplay();">
@@ -64,12 +65,12 @@
     </main>
 
     <!-- Snackbar Notifications -->
-    <div id="snackbar">asd</div>
+    <div id="snackbar"><?= translate("✅ URL has been copied to clipboard!") ?></div>
 
     <!-- Site Javascript -->
-    <script src="./assets/js/globalFunctions.js"></script>
-    <script src="./assets/js/buttonCopyURL.js"></script>
-    <script src="./assets/js/formContentUpdate.js"></script>
+    <script src="<?= getInstallationPath() ?>/Public/assets/js/globalFunctions.js"></script>
+    <script src="<?= getInstallationPath() ?>/Public/assets/js/buttonCopyURL.js"></script>
+    <script src="<?= getInstallationPath() ?>/Public/assets/js/formContentUpdate.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
