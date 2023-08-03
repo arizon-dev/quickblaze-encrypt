2b38b980a9b57dd956199e47d1bb2ecb8ff51cca
Axton <axton@arizon.dev>
2023-08-03 01:09:31 +0100

    commit

--- public/view.php
+++ public/view.php
@@ -13,7 +13,7 @@
     <!-- Site CSS -->
     <link href="<?= getInstallationPath(); ?>/public/assets/css/style.css" rel="stylesheet">
     <link href="<?= getInstallationPath(); ?>/public/assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Site Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
